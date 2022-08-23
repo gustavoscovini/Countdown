3bde04db025a3c8716cf43c9fc4f79e8b7b0aa14
Gustavo Scovini <gustavo_scovini@hotmail.com>
2022-08-23 11:17:40 -0300

    Second commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let countdownDate = new Date("September 22, 2022 16:00:00").getTime();
+
+var interval = setInterval(function () {
+  let now = new Date().getTime();
+  let distance = countdownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  if (1 <= days && days <= 9) {
+    days = "0" + days;
+  }
+  if (1 <= hours && hours <= 9) {
+    hours = "0" + hours;
+  }
+  if (1 <= minutes && minutes <= 9) {
+    minutes = "0" + minutes;
+  }
+  if (1 <= seconds && seconds <= 9) {
+    seconds = "0" + seconds;
+  }
+
+  document.getElementById("timer").innerHTML =
+    days + " : " + hours + " : " + minutes + " : " + seconds;
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
